104db7846ea591e2a947b0a1407934045e7c0097
Mohamed Abdulalim <megyptm@gmail.com>
2025-11-06 21:32:17 +0200

    1.0.0-alpha-phase1-local

---
+++ tests/SampleTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by Maatify.dev
+ * User: Maatify.dev
+ * Date: 2025-11-06
+ * Time: 21:30
+ * Project: maatify/rate-limiter
+ * IDE: PhpStorm
+ * https://www.Maatify.dev
+ */
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class SampleTest extends TestCase
+{
+    public function testInitialSetup(): void
+    {
+        $this->assertTrue(true, 'Environment setup is working correctly.');
+    }
+}
